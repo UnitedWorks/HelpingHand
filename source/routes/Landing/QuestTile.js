ddca3ca23a1b0b1b944a3b9e989540a84f05e466
Mark Hansen <x@markthemark.com>
2017-05-23 10:54:25 -0400

    More prominent giving header on tiles

--- source/routes/Landing/QuestTile.js
+++ source/routes/Landing/QuestTile.js
@@ -4,19 +4,26 @@ import { Match, Link } from "react-router-dom";
 export default class QuestTile extends Component {
   render() {
 
-    const widths = [400, 350, 300];
+    const widths = [300, 270, 240];
     const width = widths[Math.floor(Math.random() * widths.length)];
     return (
       <div className={"quest-tile".concat(` width-${width}`)}>
+        <div className="quest-tile__actions">
+          <Link to={`quest${this.props.match.path}${this.props.quest.id}`}>
+            <div className="header">
+              <span className="giving">Giving</span>
+              <span className="goArrow">→</span>
+            </div>
+            {Array.prototype.map.call(this.props.quest.goals, (quest, index) => {
+              return (
+                <div className="reward" key={index}>
+                  <u>{index + 1}</u>) {quest.giving.length > 75 ? `${quest.giving.substring(0,75)}...` : quest.giving}
+                </div>
+              );
+            })}
+          </Link>
+        </div>
         <iframe type="text/html" width={width} height={width*1.8} src={`https://www.youtube.com/embed/${this.props.quest.video_url}?controls=0&fs=0&loop=1&modestbranding=1&rel=0&color=white`} />
-        <Link className="quest-tile__footer" to={`quest${this.props.match.path}${this.props.quest.id}`}>
-          <span className="giving">Giving</span>
-          {Array.prototype.map.call(this.props.quest.goals, (quest, index) => {
-            return (<span className="reward" key={index}> {index != 0 ? ' + ' : ''}<br/>{quest.giving}</span>);
-          })}
-          <br/>
-          →
-        </Link>
       </div>
     )
   }
